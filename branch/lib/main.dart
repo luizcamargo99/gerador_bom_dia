9ef4b22ed6c2125314226b31cf85d7fa1fc80393
Luiz Camargo <luiz.zito@hotmail.com>
2022-03-03 19:06:42 -0300

    alteração nome arquivos page

--- branch/lib/main.dart
+++ branch/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:branch/pages/splash.dart';
+import 'package:branch/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
--- branch/lib/pages/splash_page.dart
+++ branch/lib/pages/splash_page.dart
@@ -1,5 +1,5 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:branch/pages/home.dart';
+import 'package:branch/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 
